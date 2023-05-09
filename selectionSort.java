ef24fc5c59d33ca86a34c736f99a9a143972bcdf
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-05-09 11:49:01 +0530

    Day 3

---
+++ selectionSort.java
@@ -0,0 +1,39 @@
+public class selectionSort {public static void printArray( int arr[]){
+
+
+    for (int i = 0 ; i <arr.length;  i ++){
+          System.out.print(arr[i]+" ");
+         }
+             System.out.println();
+       
+ 
+ }
+    public static void main (String[] args){
+        int arr[]= { 6,9,8,2,1,4,0};
+
+        // selection sort
+        for ( int i = 0 ; i < arr.length-1; i++){
+              int smallest = i;
+              for ( int j = i+1 ; j < arr.length;j++){ 
+
+
+                if ( arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+
+
+
+            }
+                    int temp = arr[smallest];
+                     arr[smallest]= arr[i];
+                    arr[i]= temp;
+            
+        }
+
+
+
+        printArray(arr);
+
+     }
+    
+}
