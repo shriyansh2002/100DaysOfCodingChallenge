f83510f8ca67b1e64965976861f1eb16a09a676e
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-05-17 22:13:28 +0530

    Day 11

---
+++ arrays.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class arrays {
+    public static void main (String[] args){
+        // int marks[]= new int [3];
+        // marks [0]= 99;
+        // marks [1]= 98;
+        // marks [2]= 9;
+        //   for (int i =0 ;i < 3 ; i++ ){
+        //     System.out.println (marks[i]);
+        //   }
+
+
+        Scanner sc= new Scanner ( System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int [size]; 
+
+        
+
+        for (int i =0 ;i < size ; i++ ){
+            
+             numbers[i]= sc.nextInt();
+        }
+
+
+        int x = sc.nextInt();
+
+
+        for (int i =0 ;i < size ; i++ ){
+            if (numbers[i]==x){
+                 System.out.println ("the index found at:"  +i );
+            }
+              }
+    
+
+
+
+
+
+
+
+    }
+    
+}
