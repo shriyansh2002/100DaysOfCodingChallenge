90ca40902b34e45f5a27c849d3b4fe9ace381f0a
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-05-18 23:16:11 +0530

    Day 12

---
+++ basicRecursion2.java
@@ -0,0 +1,14 @@
+public class basicRecursion2 {
+    public static void printFactorial(int n, int fact) {
+        if(n == 0) {
+        System.out.println(fact);
+        return;
+        }
+        fact *= n;
+        printFactorial(n-1, fact);
+        }
+        public static void main(String args[]) {
+        printFactorial(8, 1);
+        }
+    
+}
