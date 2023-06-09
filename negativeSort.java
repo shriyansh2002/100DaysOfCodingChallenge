f1c55f8fc2a695080ea04a06e90b1aeb709ace85
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-06-09 21:19:33 +0530

    Day 34

---
+++ negativeSort.java
@@ -0,0 +1,21 @@
+ import java.util.*;
+public class negativeSort {
+   
+
+    
+    public static void move(int[] arr)
+    {
+        Arrays.sort(arr);
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        int[] arr = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+        move(arr);
+        for (int e : arr)
+            System.out.print(e + " ");
+    }
+}
+    
+
