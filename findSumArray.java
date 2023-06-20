77c05a01ea93ef2639564b2e83cc385678eab0ce
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-06-20 23:28:39 +0530

    Day 45

---
+++ findSumArray.java
@@ -0,0 +1,27 @@
+public class findSumArray {
+
+	public static void main(String args[])
+	{
+		int[] arr = { 1, 5, 7, -1, 5 };
+		int sum = 8;
+		getPairsCount(arr, sum);
+	}
+
+	
+	public static void getPairsCount(int[] arr, int sum)
+	{
+
+		int count = 0; 
+		for (int i = 0; i < arr.length; i++)
+			for (int j = i + 1; j < arr.length; j++)
+				if ((arr[i] + arr[j]) == sum)
+					count++;
+
+		System.out.printf("Count of pairs is %d", count);
+	}
+}
+
+
+
+    
+
