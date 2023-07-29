667656fd511a86c72e2cb295227e4145b17a8ed6
Shriyansh Agarwal <114793749+shriyansh2002@users.noreply.github.com>
2023-07-29 22:29:15 +0530

    Day 81

---
+++ tripletSum.java
@@ -0,0 +1,42 @@
+public class tripletSum {
+    
+
+	
+	boolean find3Numbers(int A[], int arr_size, int sum)
+	{
+		int l, r;
+
+		
+		for (int i = 0; i < arr_size - 2; i++) {
+
+			
+			for (int j = i + 1; j < arr_size - 1; j++) {
+
+				
+				for (int k = j + 1; k < arr_size; k++) {
+					if (A[i] + A[j] + A[k] == sum) {
+						System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]);
+						return true;
+					}
+				}
+			}
+		}
+
+		
+		return false;
+	}
+
+
+	public static void main(String[] args)
+	{
+		tripletSum triplet = new tripletSum();
+		int A[] = { 1, 4, 45, 6, 10, 8 };
+		int sum = 22;
+		int arr_size = A.length;
+
+		triplet.find3Numbers(A, arr_size, sum);
+	}
+}
+
+    
+
